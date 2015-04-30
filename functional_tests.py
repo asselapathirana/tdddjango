97a934a4f832c1d4a9fa119978662ddc44463fc3
Assela Pathirana <assela@pathirana.net>
2015-04-30 21:47:07 +0200

    Now the table retains the first list item properly. But not yet the subsequent

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.keys import Keys 
+
 
 class NewVisitorTest(unittest.TestCase): #
     def setUp(self):
@@ -7,6 +9,12 @@ class NewVisitorTest(unittest.TestCase): #
         self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr') 
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
     
         # Edith has heard about a cool new online to-do app. She goes
@@ -15,7 +23,7 @@ class NewVisitorTest(unittest.TestCase): #
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title) #       
         # She is invited to enter a to-do item straight away
-        inputbox=self.browser.find_element_by_id(id_new_item)
+        inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item'
                          )
@@ -26,13 +34,21 @@ class NewVisitorTest(unittest.TestCase): #
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue( any(row.text == '1: Buy peacock feathers' for row in rows)
-                    )     
+              
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')    
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        # The page updates again, and now shows both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        
+        self.fail("finish the test")
         # The page updates again, and now shows both items on her list
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -14,6 +15,20 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html_page(self):
         request=HttpRequest()
         response=home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>')) #
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.strip().endswith(b'</html>'))        
+        expected=render_to_string('home.html')
+        self.assertEqual(response.content.decode(),expected)
+        
+    def test_home_page_can_save_a_POST_request(self):
+        #Setup
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text'] = 'A new list item'
+        #do-it
+        response=home_page(request)
+        #assert
+        self.assertIn("A new list item", response.content.decode())
+          #now directly render home.html with the valueset.
+        expected = render_to_string('home.html',{'new_item_text': 'A new list item'})
+          #is the response of home_page equal to this?
+        print (response.content.decode())
+        self.assertEqual(response.content.decode(), expected)
