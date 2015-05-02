5705ccd009861a91fa862e4a6a3f74298e8e601f
Assela Pathirana <assela@pathirana.net>
2015-05-02 18:00:41 +0200

    Now new list item processing is in list not home

--- lists/tests.py
+++ lists/tests.py
@@ -7,6 +7,24 @@ from lists.models import Item
 
 # Create your tests here.
 
+class NewListTest(TestCase):
+    def test_saving_POST_request(self):
+        self.client.post(
+        '/lists/new',
+        data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')                
+        
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+        '/lists/new',
+        data={'item_text': 'A new list item'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+    
+
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
@@ -49,27 +67,6 @@ class HomePageTest(TestCase):
         expected=render_to_string('home.html')
         self.assertEqual(response.content.decode(),expected)
         
-    def test_home_page_can_save_a_POST_request(self):
-        #Setup
-        t='A new list item'
-        request=HttpRequest()
-        request.method='POST'
-        request.POST['item_text'] = t
-        #do-it
-        response=home_page(request)
-        #assert
-        #first via Item
-        self.assertEqual(Item.objects.count(),1) # should have 1 item
-        new_item=Item.objects.first()
-        self.assertEqual(new_item.text,t)        
-        
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'A new list item'
-        response = home_page(request)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')        
         
     def test_home_page_only_saves_items_when_necessary_ie_POSTed(self):
         request = HttpRequest()
