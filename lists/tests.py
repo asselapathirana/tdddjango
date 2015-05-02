cf046b5c8b11b623f0ee7039f9e12e549886e7a6
Assela Pathirana <assela@pathirana.net>
2015-05-02 18:25:59 +0200

    List creation works

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 
@@ -31,27 +31,42 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')    
 
     def test_display_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        _list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=_list)
+        Item.objects.create(text='itemey 2', list=_list)
       
         response = self.client.get('/lists/the-only-list-in-the-world/') #
         
         self.assertContains(response, 'itemey 1') #
         self.assertContains(response, 'itemey 2')     
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
-        t=('The first ever item.', 'The second item now$.', '%#LXED KL thrid')
-        for text in t:
-            it = Item()
-            it.text = text
-            it.save()
-        noit = Item.objects.all() 
-        self.assertEqual(len(noit),len(t))
+        list_ = List()
+        list_.save()
         
-        for i,tt in enumerate(t):
-            self.assertEqual(noit[i].text,tt)
-            
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+        
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+    
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)        
  
 
 class HomePageTest(TestCase):
