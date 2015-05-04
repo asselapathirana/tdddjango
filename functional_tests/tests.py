712fa27846c9d3ca7ce000c8f34bb454fb11c6e0
unknown <apa@1403HPL-002.education.ihe.nl>
2015-05-04 16:57:11 +0200

    fixed few mistakes (minor) on base.html

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(StaticLiveServerTestCase): #
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        #self.browser.implicitly_wait(3)
     def tearDown(self):
         self.browser.refresh()
         self.browser.quit()
