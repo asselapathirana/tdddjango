17c215073af082d806d5a360bd8ed49dc0e24038
Assela Pathirana <assela@pathirana.net>
2015-05-09 16:57:04 +0200

    tried in a different computer. updated README

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.keys import Keys 
 import sys
+from unittest import skip
 
 
 class NewVisitorTest(StaticLiveServerTestCase): #
@@ -31,6 +32,17 @@ class NewVisitorTest(StaticLiveServerTestCase): #
         rows = table.find_elements_by_tag_name('tr') 
         self.assertIn(row_text, [row.text for row in rows])
         
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+        # The home page refreshes, and there is an error message saying
+        # that list items cannot be blank
+        # She tries again with some text for the item, which now works
+        # Perversely, she now decides to submit a second blank list item
+        # She receives a similar warning on the list page
+        # And she can correct it by filling some text in
+        self.fail('write me!')        
         
     def test_layout_and_styling(self):
         # Edith goes to the home page
