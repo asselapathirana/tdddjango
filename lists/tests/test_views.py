cb119579e45eed61dfd85c7ca3eff055abe3b96d
Assela Pathirana <assela@pathirana.net>
2015-05-10 21:26:03 +0200

    lists/id/ can handle post requests. No empty values accepted. All tests pass

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -70,7 +70,16 @@ class ListViewTest(TestCase):
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
-        self.assertEqual(Item.objects.count(), 0)    
+        self.assertEqual(Item.objects.count(), 0) 
+        
+    def test_invalid_list_items_arent_saved_existing_list(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)  
+        self.assertEqual(List.objects.count(), 1)
+        self.assertEqual(Item.objects.count(), 1)        
+        self.client.post('/lists/%d/' % (list_.id), data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 1)
+        self.assertEqual(Item.objects.count(), 1)    
     
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
