ea19b9ef6a93e7435e21159567e886b840ed5f36
Assela Pathirana <assela@pathirana.net>
2015-05-10 13:20:50 +0200

    FT for empty list item. fails as expected. UT are passing

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,12 +1,15 @@
 from .base import FunctionalTest
 from unittest import skip
+from selenium.common.exceptions import NoSuchElementException
 class ItemValidationTest(FunctionalTest):
-    @skip
+    #@skip
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.server_url)
-        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertRaises(NoSuchElementException,
+            self.browser.find_element_by_css_selector,'.has-error')
+
         self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
@@ -15,8 +18,19 @@ class ItemValidationTest(FunctionalTest):
         # She tries again with some text for the item, which now works
         self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
         self.check_for_row_in_list_table('1: Buy milk') 
+        self.assertRaises(NoSuchElementException,
+            self.browser.find_element_by_css_selector,'.has-error')        
+        
         
         # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # She receives a similar warning on the list page
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
         # And she can correct it by filling some text in
-        self.fail('write me!') 
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea') 
+        self.assertRaises(NoSuchElementException,
+                          self.browser.find_element_by_css_selector,'.has-error')        
