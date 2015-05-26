b8cd646d702493f12e7301bb433fbca11646ebce
Assela Pathirana <assela@pathirana.net>
2015-05-26 21:26:43 +0200

    sslify settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
